c4267f5370f11c7fa90e5afaa4dda34a12e8fcb8
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-12-13 14:27:20 +0530

    Create movie.model.js

---
+++ src/models/movie.model.js
@@ -0,0 +1,17 @@
+const { Schema , model } = require("mongoose");
+
+const movieSchema = new Schema ({
+
+movie_name:{type: String , required:true},
+actors:[{type: String , required:true }],
+language:[{type: String , required:true }],
+director:[{type: String , required:true }],
+movie_poster:[{type: String , required:true }],
+
+},
+{
+    versionKey:false,
+    timestamps:true
+});
+
+module.exports = model("movie" , movieSchema);
