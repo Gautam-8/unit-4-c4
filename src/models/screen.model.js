ca8eeb372c9b504d8efe1ac037ac4fccd6d260a3
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-12-13 14:27:22 +0530

    Create screen.model.js

---
+++ src/models/screen.model.js
@@ -0,0 +1,14 @@
+const { Schema , model } = require("mongoose");
+
+const screenSchema = new Schema ({
+
+cinema_name:{type: String , required:true},
+theatre:{type: Schema.Types.ObjectId , ref : "theatre", required:true },
+
+},
+{
+    versionKey:false,
+    timestamps:true
+});
+
+module.exports = model("screen" ,screenSchema);
